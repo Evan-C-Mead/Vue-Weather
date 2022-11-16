42b0a142aa5efbb2ea100a367d5e90edcdc4ca3f
Evan-C-Mead <evan.c.mead@gmail.com>
2022-11-16 10:29:43 -0600

    understanding styling and public dir access

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <h1>Hello there!</h1>
+    <main>
+      <h1>Vue-Weather</h1>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search weather forecast"/>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -10,7 +15,7 @@ import WeatherKey from './keys/open-weather.txt'
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+
   },
   data() {
     return {
@@ -22,11 +27,60 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: papayawhip;
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
 }
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-bar {
+  text-align: center;
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: papayawhip;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background: rgba(255, 255, 255, 0.6) none;
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
 </style>
